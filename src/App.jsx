f48947dc07c50b850961637f8753ff31f118795e
dhruvxop19 <dhruvxop19@gmail.com>
2025-08-16 11:18:03 +0530

    conditionally render and remove time from 2nd div

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,6 @@ function App() {
             <PostComponent
             name = {"raman"}
             subtitile = {"promoted"}
-            time={"4m ago"}
             image = {"https://appx-wsb-gcp-mcdn.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg"}
             description = {"How to get hired in 2025? I lost my job in 2024, this is the roadmap I followed to get a job "}
              />
@@ -53,11 +52,13 @@ function  PostComponent({name,subtitile,time,image, description}) {
           {name}
         </b>
         <div>{subtitile}</div>
-        <div style={{display : 'flex' }}>      
+        {( time !== undefined ) ? <div style=
+        {{display : 'flex' }}>      
         <div>{time}</div>
         <img src="https://media.istockphoto.com/id/931336618/vector/clock-vector-icon-isolated.
         jpg?s=612x612&w=0&k=20&c=I8EBJl8i6olqcrhAtKko74ydFEVbfCQ6s5Pbsx6vfas=" style={{width : 12 , height: 12}} />
-        </div>
+
+        </div> : null }
       </div>
 </div>
        <div style={{fontSize : 12}}>
