2a6d9ec52b943fc7a1b2c9b87a08bef93f44e437
dhruvxop19 <dhruvxop19@gmail.com>
2025-08-16 20:05:18 +0530

    step 1 add posts array

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,14 @@ import { PostComponent } from "./post";
 
 function App() {
 
+  const posts = [{
+    name : "harkirat",
+    subtitile : "1000 followers",
+    time : "2m ago",
+    image : "https://appx-wsb-gcp-mcdn.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg",
+    description : "Want to know how to win big ? Check out how these folks won $6000 in bounties"
+  }]
+
   function addPost() {
 
   }
