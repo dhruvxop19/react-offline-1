f46f76455a92ca5e276c6c4c745f1f93fd8e1f4e
dhruvxop19 <dhruvxop19@gmail.com>
2025-08-16 09:56:22 +0530

    props added

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,28 @@
 function App() {
 
   return (
+   
     <div style={{background: "#dfe6e9", height: "100vh"}}>
       <div style={{display : "flex", justifyContent: "center"}}>
         <div>
           <div>
-            <PostComponent/>
+            <PostComponent
+            name = {"harkirat"}
+            subtitile = {"20 followers"}
+            time = {"2m ago"}
+            image = {"https://appx-wsb-gcp-mcdn.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg"}
+            description = {" Want to know how to win big ? Check out how these folks won $6000 in bounties "}
+            />
             <br />
           </div>
           <div>
-            <PostComponent/>
+            <PostComponent
+            name = {"raman"}
+            subtitile = {"promoted"}
+            time={"4m ago"}
+            image = {"https://appx-wsb-gcp-mcdn.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg"}
+            description = {"How to get hired in 2025? I lost my job in 2024, this is the roadmap I followed to get a job "}
+             />
             <br />
           </div>
           <div>
@@ -27,24 +40,28 @@ function App() {
 const style = { width: 200, backgroundColor: "white", borderRadius: 10, borderColor: "gray", borderWidth: 1 , padding: 20
 }
 
-function  PostComponent() {
+function  PostComponent({name,subtitile,time,image, description}) {
   return <div style={style}>
      <div style={{display: "flex"}}> 
-      <img src={"https://appx-wsb-gcp-mcdn.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg"} style={{
+      <img src={image} style={{
         width: 30,
         height: 30,
         borderRadius: 20
       }} />
       <div style={{fontSize: 10 , marginLeft: 10}}>
         <b>
-          100xdevs
+          {name}
         </b>
-        <div>23,888 followers</div>
-        <div>12m</div>
+        <div>{subtitile}</div>
+        <div style={{display : 'flex' }}>      
+        <div>{time}</div>
+        <img src="https://media.istockphoto.com/id/931336618/vector/clock-vector-icon-isolated.
+        jpg?s=612x612&w=0&k=20&c=I8EBJl8i6olqcrhAtKko74ydFEVbfCQ6s5Pbsx6vfas=" style={{width : 12 , height: 12}} />
+        </div>
       </div>
 </div>
        <div style={{fontSize : 12}}>
-        Want to know how to win big ? Check out how these folks won $6000 in bounties 
+        {description}
 
        </div>
        </div>
